555b4a9d3aa6740a93292d730f842d0798c8ed57
Varun <varunkukade999@gmail.com>
2023-04-10 23:50:10 +0530

    Added profile picture updation feature

--- src/components/welcomeComponent.tsx
+++ src/components/welcomeComponent.tsx
@@ -1,10 +1,51 @@
-import React, {ReactElement} from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {ReactElement, useState} from 'react';
+import {
+  Alert,
+  Platform,
+  StyleSheet,
+  ToastAndroid,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import ImageComponent from '../reusables/imageComponent';
 import TextComponent from '../reusables/textComponent';
 import {colors} from '../utils/appStyles';
+import * as ImagePicker from 'react-native-image-picker';
+import auth from '@react-native-firebase/auth';
 
 const WelcomeComponent = (): ReactElement => {
+  const [uri, setUri] = useState('');
+  const onProfilePictureClick = () => {
+    ImagePicker.launchImageLibrary({mediaType: 'photo'}, response => {
+      if (response.didCancel) {
+      } else if (response.errorCode) {
+        Alert.alert('ImagePicker Error: ', response.errorMessage);
+      } else if (response.assets && response.assets.length > 0) {
+        auth()
+          .currentUser?.updateProfile({
+            photoURL: response.assets[0]?.uri || '',
+          })
+          .then(res => {
+            if (response.assets && response.assets.length > 0)
+              setUri(response.assets[0]?.uri || '');
+            if (Platform.OS === 'android') {
+              ToastAndroid.show(
+                'Profile picture Updated successfully.',
+                ToastAndroid.SHORT,
+              );
+            }
+          })
+          .catch(err => {
+            if (Platform.OS === 'android') {
+              ToastAndroid.show(
+                'Failed to update Profile picture. Please try again later.',
+                ToastAndroid.SHORT,
+              );
+            }
+          });
+      }
+    });
+  };
   return (
     <View style={styles.welcomeComponent}>
       <View style={styles.welcomeHelloText}>
@@ -19,12 +60,22 @@ const WelcomeComponent = (): ReactElement => {
           Varun Kukade
         </TextComponent>
       </View>
-      <View style={styles.profilePicContainer}>
+      <TouchableOpacity
+        onPress={onProfilePictureClick}
+        activeOpacity={0.6}
+        style={styles.profilePicContainer}>
         <ImageComponent
-          source={require('../../images/profilePic.jpg')}
+          source={
+            auth().currentUser?.photoURL
+              ? {uri: auth().currentUser?.photoURL}
+              : require('../../images/dummyPicture.png')
+          }
           style={{width: 60, height: 60, borderRadius: 30}}
         />
-      </View>
+        <TextComponent style={{color: colors.greyColor}} weight="extraBold">
+          Update
+        </TextComponent>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -33,17 +84,19 @@ export const MemoizedWelcomeComponent = React.memo(WelcomeComponent);
 
 const styles = StyleSheet.create({
   welcomeComponent: {
-    flex: 0.1,
+    flex: 0.15,
     width: '100%',
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
   },
   welcomeHelloText: {
     width: '70%',
   },
   profilePicContainer: {
     width: '30%',
-    alignItems: 'flex-end',
+    alignItems: 'center',
+    justifyContent: 'center',
     height: '100%',
   },
 });
